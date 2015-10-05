75e975f608b615845d2ed4ab7975dd234430d9a9
Luis <luisbaugusto@gmail.com>
2015-10-04 18:01:40 -0700

    working on showing and hiding UI text

--- Assets/CutsceneSkipApi/CutsceneSkipApi.cs
+++ Assets/CutsceneSkipApi/CutsceneSkipApi.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace Adnc.Cutscene
 {
@@ -12,15 +13,20 @@ namespace Adnc.Cutscene
 
         [SerializeField]
         string skipAction;
+		KeyCode skipActionCode;
 
         public void BeginMonitor ()
         {
+			//At the start of a cutscene, run this script to begin monitoring for button pressing
+
             Debug.Log("Starting monitor");
             StartCoroutine("LoopMonitor");
         }
 
         public void EndMonitor()
         {
+			//Run this script either at the end of a cutscene or if the scene was skipped with a button press
+
             Debug.Log("Ending monitor");
             StopCoroutine("LoopMonitor");
         }
@@ -32,7 +38,9 @@ namespace Adnc.Cutscene
 
             while (!Input.anyKey) yield return null;
 
-            Debug.Log("A key or mouse click has been detected. Press 'A' to stop the monitor.");
+            Debug.Log("A key or mouse click has been detected. Press '" + skipAction + "' to stop the monitor.");
+
+			StartCoroutine ("ShowUIText");
 
             yield return new WaitForSeconds(0.1f);
             float timer = displayTime;
@@ -40,11 +48,11 @@ namespace Adnc.Cutscene
             while (timer > 0)
             {
 
-                if (Input.GetKey(KeyCode.A))
+				//Replace keycode with an input from Rewired
+				if (Input.GetKey(skipActionCode))
                 {
-                    Debug.Log("'A' has been pressed.");
+                    Debug.Log("'" + skipAction + "' has been pressed.");
                     SkipScene();
-                    EndMonitor();
                     break;
                 }
 
@@ -58,20 +66,75 @@ namespace Adnc.Cutscene
             } 
             else
             {
-                Debug.Log("The 'A' key was never pressed. Going back to checking for any key press.");
-                yield return StartCoroutine("LoopMonitor");
+                Debug.Log("The " + skipAction + " key was never pressed. Going back to checking for any key press.");
+
+				StartCoroutine ("HideUIText");
+								
+				yield return StartCoroutine("LoopMonitor");
             }
 
         }
 
         public void SkipScene ()
         {
-            Debug.Log("Skip the scene.");
+
+			Debug.Log("Skip the scene.");
+						
+			EndMonitor();
+			StartCoroutine ("HideUIText");
+
+			//Add code to end scene via Playmaker
         }
+
+		IEnumerator ShowUIText() 
+		{
+
+			Text text = GetComponent<Text> ();
+			float currentTime = 0;
+
+			while (text.color.a < 1f) 
+			{
+
+				currentTime += Time.deltaTime;
+
+				float alpha = Mathf.Lerp(text.color.a, 1f, 1f * currentTime/3f);
+				text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+				yield return new WaitForEndOfFrame();
+
+			}
+
+			text.color = Color.white;
+
+			yield return null;
+
+		}
+
+		IEnumerator HideUIText()
+		{
+			Debug.Log ("Hiding Text");
+			StopCoroutine ("ShowUIText");
+
+			Text text = GetComponent<Text> ();
+			float currentTime = 0;
+
+			while (text.color.a > 0f) 
+			{
+				currentTime += Time.deltaTime;
+
+				float alpha = Mathf.Lerp(1f, 0f, 1f * currentTime/3f);
+				text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+				yield return new WaitForEndOfFrame();
+				
+			}
+
+			yield return null;
+
+		}
 
         //delete after testing
         public void Start()
-        {
+        {	
+			skipActionCode = (KeyCode)System.Enum.Parse (typeof(KeyCode), skipAction);
             BeginMonitor();
         }
 
